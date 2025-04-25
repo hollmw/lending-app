0097c24684f776a271e5a2c941960fa9af387fd3
HOLLMW <maxholloway1912@gmail.com>
2025-04-25 22:52:08 +0800

    removed useless import

--- backend/src/main/java/com/defi/service/web3service.java
+++ backend/src/main/java/com/defi/service/web3service.java
@@ -10,7 +10,6 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.StaticGasProvider;
 import org.web3j.tx.gas.DefaultGasProvider;
-import org.web3j.tx.exceptions.ContractCallException;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 import jakarta.annotation.PostConstruct;
