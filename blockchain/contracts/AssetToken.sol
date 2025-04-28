86f3612c14299554172f3c1eb88bb47426abcd8f
HOLLMW <maxholloway1912@gmail.com>
2025-04-28 23:15:09 +0800

    assets show up in dashboard

--- blockchain/contracts/AssetToken.sol
+++ blockchain/contracts/AssetToken.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AssetToken is ERC721, Ownable {
+contract AssetToken is ERC721Enumerable, Ownable {
     uint256 private _tokenIdCounter;
+
     mapping(uint256 => string) private _tokenURIs;
 
-    constructor() ERC721("AssetToken", "AST") Ownable() {}  // Added Ownable constructor
+    constructor() ERC721("AssetToken", "AST") {}
 
-    function mint(address to, string memory tokenURI) public returns (uint256) {
+    function mint(address to, string memory _tokenURI) public returns (uint256) {
         _tokenIdCounter++;
         uint256 newTokenId = _tokenIdCounter;
         _mint(to, newTokenId);
-        _tokenURIs[newTokenId] = tokenURI;
+        _tokenURIs[newTokenId] = _tokenURI;
         return newTokenId;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return _tokenURIs[tokenId];
     }
 }
